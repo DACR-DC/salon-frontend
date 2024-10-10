9c927460bec7e5c680db6e3bbe4cbb0cdb342ea5
David Carrillo <carrillod421@gmail.com>
2024-10-09 21:13:00 -0600

    actualizando constant

--- disenoapp/src/constant.jsx
+++ disenoapp/src/constant.jsx
@@ -1,4 +1,4 @@
 //localmente
 //export const BACKEND_API ="http://localhost:3001/"
 //vercel
-export const BACKEND_API ="salon-backend-delta.vercel.app/"
+export const BACKEND_API ="https://salon-backend-delta.vercel.app/"
